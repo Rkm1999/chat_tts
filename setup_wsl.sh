a440a420d4c680e37fff1bb23f39bfd561b238c2
Rkm1999 <junida1999@gmail.com>
2026-03-31 20:52:24 -0700

    updated setup script

--- setup_wsl.sh
+++ setup_wsl.sh
@@ -4,6 +4,10 @@ VENV="$HOME/chat_tts_venv"
 REPO="$(cd "$(dirname "$0")" && pwd)"
 PIP="$VENV/bin/pip"
 
+echo '--- Creating venv ---'
+python3 -m venv "$VENV"
+echo 'VENV_OK'
+
 echo '--- Upgrading pip ---'
 $PIP install --upgrade pip -q
 
@@ -18,3 +22,6 @@ echo 'REQS_OK'
 echo '--- Installing qwen3-tts-triton ---'
 $PIP install qwen3-tts-triton==0.1.0 --ignore-requires-python --no-deps -q
 echo 'TRITON_OK'
+
+echo ''
+echo 'Done! Activate with: source ~/chat_tts_venv/bin/activate'
